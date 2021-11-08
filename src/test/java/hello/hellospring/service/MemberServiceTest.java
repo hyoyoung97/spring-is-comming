e1d1fead70a46f807cc96d54bae4ef538e2ee27e
hyoyoung97 <hyoyoung97@naver.com>
2021-11-08 11:45:29 +0900

    first1 commit

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
-    //테스트 코드는 한글로도 많이 적음, 빌드될 때 포함되지 않음 
+    //테스트 코드는 한글로도 많이 적음, 빌드될 때 포함되지 않음
+
+
+    
     @Test
     void 회원가입() {
     }
